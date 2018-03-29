99112637ef37a13105084d25a31d021d1411bd7e
Shun Yao <ysmiracle@hotmail.com>
2018-03-29 06:19:05 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,14 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  
+  holiday_hash.each do |key1, value1|
+    puts key1.to_s.capitalize! + ":"
+      value1.each do |key2, value2|
+        puts key2.to_s.split("_").capitalize!.join(" ") + ":"
+        puts value2.join(" ")
+      end
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
